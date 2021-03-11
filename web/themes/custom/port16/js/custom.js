ced414d5cb33d0373a746cf02ab76bd0219661e3
DDEV-Local User <nobody@example.com>
2021-03-11 03:45:50 +0000

    c11-theme-wa: Created Port16 theme.

---
+++ web/themes/custom/port16/js/custom.js
@@ -0,0 +1,18 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+(function($, Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.port16 = {
+    attach: function(context, settings) {
+
+      // Custom code here
+
+    }
+  };
+
+})(jQuery, Drupal);
