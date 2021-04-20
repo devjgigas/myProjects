2830ccaa0f74915afaca5952e0a20e53cca1236c
Marissa Samala <msamala@manhattanstrategy.com>
2021-04-20 19:14:14 -0400

    Created copyright block, enabled port_core module.

---
+++ web/modules/custom/port_core/src/Plugin/Block/CopyrightBlock.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Drupal\port_core\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * Provides a copyright block.
+ *
+ * @Block(
+ *   id = "port_core_copyright",
+ *   admin_label = @Translation("Copyright"),
+ *   category = @Translation("Custom")
+ * )
+ */
+class CopyrightBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $build['content'] = [
+      '#markup' => $this->t('It works!'),
+    ];
+    return $build;
+  }
+
+}
